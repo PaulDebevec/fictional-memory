f5a7782ee433ef93a85b8d7d07d846c498e54e01
Paul Debevec <paulmdebevec@gmail.com>
2021-02-22 18:13:31 -0700

    Create SessionsController with create method.

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V0::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      user.generate_api_token
+      message = "user authenticated"
+      render json: {'auth_token' => user.auth_token, 'message' => message}, status: 200
+    else
+      message = "Invalid user credentials"
+    end
+  end
+end
