16113d364110bba8ed7c8ca55c8e82d5be29c218
Paul Debevec <paulmdebevec@gmail.com>
2021-02-23 12:39:50 -0700

    Refactor Player validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,4 @@
 class Player < ApplicationRecord
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :height
-  validates_presence_of :weight
-  validates_presence_of :birthday
-  validates_presence_of :graduation_year
-  validates_presence_of :position
+  validates_presence_of :first_name, :last_name, :height, :weight, :birthday,
+                        :graduation_year, :position
 end
