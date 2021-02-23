ea21898ed923756ac71f4d59b92c302347a09ee9
Paul Debevec <paulmdebevec@gmail.com>
2021-02-22 18:11:36 -0700

    Refactor test change api_token to auth_token. Change body payload to params

--- spec/requests/api/v0/login_request_spec.rb
+++ spec/requests/api/v0/login_request_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User Login Endpoint Request' do
-  it 'A successful response returns an api_token with a 200 status' do
+  it 'A successful response returns an auth_token with a 200 status' do
     user = User.create( email: 'sample.coach@mobile.edu',
                         password: 'samplepassword')
 
-    post '/api/v0/login', body: { email: user.email,
+    post '/api/v0/login', params: { email: user.email,
                                   password: user.password}
 
-
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:message]).to eq("user authenticated")
+    expect(json[:auth_token]).not_to eq(nil)
+    expect(json.keys.include?(:auth_token)).to eq(true)
+    expect(json.keys.include?(:message)).to eq(true)
   end
 end
