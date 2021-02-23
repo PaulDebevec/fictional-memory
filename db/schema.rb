6cbc2df9c9074d9f0840117b267b952b22bd4416
Paul Debevec <paulmdebevec@gmail.com>
2021-02-23 12:04:44 -0700

    Create migration for Player. Add player to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_205614) do
+ActiveRecord::Schema.define(version: 2021_02_23_183448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "height"
+    t.integer "weight"
+    t.string "birthday"
+    t.integer "graduation_year"
+    t.string "position"
+    t.boolean "recruit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
