e87c9a01a0510ea33d9f91773bb2af3feaf7e332
Paul Debevec <paulmdebevec@gmail.com>
2021-02-23 12:54:55 -0700

    Add has_many relationship to team. Add belongs_to (optional) to player.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,6 @@ class Team < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :age_group, require: false
   validates_presence_of :coach, require: false
+
+  has_many :players
 end
