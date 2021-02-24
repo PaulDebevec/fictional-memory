530d0b392c355c243f93a8e78b38c81df881b0bc
Paul Debevec <paulmdebevec@gmail.com>
2021-02-23 20:14:02 -0700

    Add return value for `/players` endpoint along with helper methods

--- app/controllers/api/v0/players_controller.rb
+++ app/controllers/api/v0/players_controller.rb
@@ -1,4 +1,6 @@
 class Api::V0::PlayersController < ApplicationController
   def index
+    players = current_user.team.players
+    render json: PlayersSerializer.new(players) if valid_auth_token?
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :valid_auth_token?
+
+  def current_user
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def valid_auth_token?
+    return true if current_user.auth_token != nil
+  end
 end
