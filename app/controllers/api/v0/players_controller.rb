0f661d2753f82edb5bd2ed7b9f5c6cab374bde21
Paul Debevec <paulmdebevec@gmail.com>
2021-02-24 13:07:03 -0700

    Refactor PlayerController index method for multiple access points

--- app/controllers/api/v0/players_controller.rb
+++ app/controllers/api/v0/players_controller.rb
@@ -1,6 +1,16 @@
 class Api::V0::PlayersController < ApplicationController
   def index
-    players = current_user.team.players
-    render json: PlayersSerializer.new(players) if valid_auth_token?
+    user_in_session = User.find_by(id: session[:user_id])
+    if user_in_session.nil? && valid_auth_token?
+      coach = User.find_by(auth_token: request.headers['auth_token'])
+      players = coach.team.players
+      render json: PlayersSerializer.new(players)
+    elsif user_in_session != nil
+      players = user_in_session.team.players
+      render json: PlayersSerializer.new(players) if valid_auth_token?
+    else
+      message = "You do not have access to view this page"
+      render json: {'data': { 'message': message}}, status: 401
+    end
   end
 end
