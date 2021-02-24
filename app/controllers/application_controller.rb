b077a1062ccad90817197b6fb7334e56918c5567
Paul Debevec <paulmdebevec@gmail.com>
2021-02-24 13:06:26 -0700

    Refactor valid_auth_token? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   helper_method :current_user, :valid_auth_token?
 
   def current_user
@@ -6,6 +7,9 @@ class ApplicationController < ActionController::Base
   end
 
   def valid_auth_token?
-    return true if current_user.auth_token != nil
+    return true if current_user&.auth_token
+
+    token = request.headers['auth_token'] || request.headers['HTTP_AUTH_TOKEN']
+    return true if User.find_by(auth_token: token)
   end
 end
