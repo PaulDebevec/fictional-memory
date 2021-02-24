81623347a393b0d70c33e02f2b0c427d0353076e
Paul Debevec <paulmdebevec@gmail.com>
2021-02-24 13:05:53 -0700

    Add headers auth for Postman testing

--- spec/requests/api/v0/player_request_spec.rb
+++ spec/requests/api/v0/player_request_spec.rb
@@ -26,7 +26,45 @@ RSpec.describe 'User Login Endpoint Request' do
     post '/api/v0/login', params: payload
 
     # Begin test for '/players' endpoint
-    get '/api/v0/players'
+    payload_2 = { auth_token: coach_and_team_1.auth_token, message: 'user authorized' }.to_json
+
+    get '/api/v0/players', params: payload_2
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][0].keys.include?(:id)).to eq(true)
+    expect(json[:data][0].keys.include?(:type)).to eq(true)
+    expect(json[:data][0].keys.include?(:attributes)).to eq(true)
+    expect(json[:data][0][:type]).to eq('players')
+    expect(json[:data][0][:attributes].keys.include?(:first_name)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:last_name)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:height)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:weight)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:birthday)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:graduation_year)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:position)).to eq(true)
+    expect(json[:data][0][:attributes].keys.include?(:recruit)).to eq(true)
+    expect(json[:data][0][:attributes].length).to eq(8)
+    expect(json[:data].length).to eq(5)
+  end
+
+  it 'API sends responds to an auth_token being sent through the headers of the request' do
+    # Create a user with random name, random email, and password of 'password'
+    coach_and_team_1 = FactoryBot.create(:user , team_id: FactoryBot.create(:team).id)
+    coach_and_team_1.team.update(coach: coach_and_team_1.full_name)
+    # Update user email to 'samplecoach@example.com'
+    coach_and_team_1.update(auth_token: 'NHa/tjwQOJxDTUzG1XTSYvgr0ru9iqCcXsG3Ovwl8OjcVOE8B67zMHrhcn+z34Lf1HXIb1WeDOoFyGn8+58Q26ookmMaJTHVG9a19sfeEPoabKShCFSJToBM')
+    team_1 = coach_and_team_1.team
+    team_1_players = FactoryBot.create_list(:player, 5)
+    team_1.players << team_1_players
+
+    message = 'user authorized'
+    # Begin test for '/players' endpoint
+    payload_2 = { auth_token: coach_and_team_1.auth_token, message: message }
+
+    get '/api/v0/players', headers: payload_2
+
     expect(response).to be_successful
     expect(response.status).to eq(200)
     json = JSON.parse(response.body, symbolize_names: true)
